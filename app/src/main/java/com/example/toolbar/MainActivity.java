69139ba2de479f666317703850b3ab53f1579eee
EmreCogac <emremta9@gmail.com>
2024-01-25 01:53:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/toolbar/MainActivity.java
@@ -0,0 +1,42 @@
+     package com.example.toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+    public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textid);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.deneme1){
+            textView.setText("Deneme");
+            return true;
+        }else if(id == R.id.help){
+            textView.setText("Help");
+            return true;
+        }
+        return false;
+    }
+
+}
